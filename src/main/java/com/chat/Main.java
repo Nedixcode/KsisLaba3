43351ed8f3ff3c87e7739fa7002cef5056e1ee06
Nedixcode <kirilgrischa@mail.ru>
2026-03-16 16:20:18 +0300

    4 версия

--- src/main/java/com/chat/Main.java
+++ src/main/java/com/chat/Main.java
@@ -2,11 +2,15 @@ package com.chat;
 
 import com.chat.core.PeerNode;
 import com.chat.ui.ConsoleUI;
+import java.net.ServerSocket;
+import java.net.InetSocketAddress;
 
 public class Main {
-    static void main(String[] args) {
-        String name = "User"; String ip = "127.0.0.1";
-        int tcpPort = 9000; int udpPort = 8888;
+    public static void main(String[] args) {
+        String name = "User";
+        String ip = "127.0.0.1";
+        int tcpPort = 9000;
+        int udpPort = 8888;
 
         for (String arg : args) {
             if (arg.startsWith("--name=")) name = arg.substring(7);
@@ -14,8 +18,14 @@ public class Main {
             if (arg.startsWith("--tcp-port=")) tcpPort = Integer.parseInt(arg.substring(11));
         }
 
+        if (!isPortAvailable(ip, tcpPort)) {
+            System.err.println("Ошибка: Адрес " + ip + ":" + tcpPort + " уже занят другим узлом.");
+            System.exit(1);
+        }
+
         System.out.println("\nStarting peer:");
         System.out.println("Name: " + name);
+        System.out.println("IP: " + ip);
         System.out.println("TCP port: " + tcpPort);
         System.out.println("UDP port: " + udpPort);
 
@@ -23,5 +33,14 @@ public class Main {
         ConsoleUI ui = new ConsoleUI(node);
         node.start();
         ui.start();
+    }
+
+    private static boolean isPortAvailable(String ip, int port) {
+        try (ServerSocket ss = new ServerSocket()) {
+            ss.bind(new InetSocketAddress(ip, port));
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
